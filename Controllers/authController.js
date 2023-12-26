8784d8ff7df2133e44afba4cb32f54cdd3d73fe5
Benaouda Rafik <gokub531@gmail.com>
2023-12-26 14:16:08 +0100

    Update authController.js

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -4,12 +4,12 @@ import bcrypt from "bcrypt"
 import asyncWrapper from "../utils/asyncWrapper.js"
 import CustomError from "../utils/CustomError.js"
 export const register = asyncWrapper(  async function (req,res,next){
-    const {name,password} = req.body
+    const {name,password,email} = req.body
    
-    const user = await User.create({name,password})
+    const user = await User.create({name,password,email})
 
     const token = await jwt.sign({id:user._id}, process.env.JWT_SECRET,{expiresIn:'2d'})
-  const userDetail = {name: user.name , email : user.email}
+  const userDetail = {name: user.name , email : user.email,role : user.role}
     res.status(201).json({user :{...userDetail},token })
    
 })
@@ -25,7 +25,7 @@ export const login = asyncWrapper(async function login(req,res){
            throw new CustomError("invalid password!",400)
         }
         const token  = await jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"2d"})
-     const userDetail = {name: user.name , email : user.email}
+     const userDetail = {name: user.name , email : user.email, role : user.role}
         res.status(200).json({user :{...userDetail},token })
         
    
